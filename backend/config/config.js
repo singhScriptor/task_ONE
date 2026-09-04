c67a4cb5ca280c1302226f8f1a385a390f416352
starlord <dhirajkr1117@gmail.com>
2026-09-04 09:30:30 +0530

    updating config.js inside config using dotenv

--- backend/config/config.js
+++ backend/config/config.js
@@ -2,24 +2,24 @@ require('dotenv').config();
 
 module.exports = {
   development: {
-    username: "root",
-    password: process.env.LOCALHOST_PASSWORD,
-    database: "expense_tracker",
-    host: "localhost",
+    username: process.env.DB_USER||"root",
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME || "expense_tracker",
+    host: process.env.DB_HOST||"localhost",
     dialect: "mysql"
   },
   test: {
-    username: "root",
-    password: process.env.LOCALHOST_PASSWORD,
-    database: "database_test",
-    host: "localhost",
+    username: process.env.DB_USER || "root",
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_TEST||"database_test",
+    host: process.env.DB_HOST ||"localhost",
     dialect: "mysql"
   },
   production: {
-    username: "root",
-    password: process.env.LOCALHOST_PASSWORD,
-    database: "database_production",
-    host: "localhost",
+    username:process.env.DB_USER || "root",
+    password: process.env.DB_PASSWORD,
+    database:process.env.DB_PRODUCTION || "database_production",
+    host: process.env.DB_HOST || "localhost",
     dialect: "mysql"
   }
 };
